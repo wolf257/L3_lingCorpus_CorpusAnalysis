55062db8b5571b0e9330d486d43471b24d2625c6
Wolf <wolf257@users.noreply.github.com>
2017-11-10 10:04:28 +0100

    Ebauche outside_text_as_one_liste()

--- code_source/modules/traitement_texte.py
+++ code_source/modules/traitement_texte.py
@@ -3,16 +3,28 @@
 
 import re
 
-
 ##############################################################
-# Fonction : convert_utf8
-# 	- Convertit chaque caracteres en utf8
-# Input :
+# Fonction : import text as one list
+# 	-
+# Input : text
+# Return : list
 ##############################################################
-def convert_utf8():
-    pass
+def outside_text_as_one_liste(text):
+    ''' Import a text as one list '''
 
-# Caractere par car
+    liste_brute = []
+    liste_finale = []
+
+    #Importation du texte as one list.
+    #Pb : les mots contiennent le '\n'.
+    with open(text, 'r') as file :
+        liste_brute = file.readlines()
+
+    #Solution : a partir de cette liste, creer une nouvelle en enlever '\n'
+    for word in liste_brute :
+        liste_finale.append(word.strip())
+
+    return liste_finale
 
 ##############################################################
 # Fonction : punctuation_out
@@ -29,6 +41,26 @@ def punctuation_out(text):
 # txt = txt.read()
 # txt = re.sub(r'[^\w\s]','',txt)
 # body = txt.lower().split()
+
+
+##############################################################
+# Fonction : convert_utf8
+# 	- Convertit chaque caracteres en utf8
+# Input :
+##############################################################
+def convert_utf8():
+    pass
+
+# Caractere par car
+
+
+##############################################################
+# Fonction : space_before_point
+# 	- Transforme tous les lettre+'.' en lettre+'espace'+'point'
+# Input :
+##############################################################
+def space_before_point(text):
+    pass
 
 ##############################################################
 # Fonction : maj_to_min
