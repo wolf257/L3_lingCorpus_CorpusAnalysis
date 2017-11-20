4c1432088ffeaab12358f1d51d6dd68df869c7f2
Wolf <wolf257@users.noreply.github.com>
2017-11-20 18:09:07 +0100

    'import_text_as_list_of_(strings|words) + test in modules/ (2)' 	new file:   modules/test_others.py

---
+++ code_source/modules/test_others.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#-*- coding : utf8 -*-
+
+import unittest
+
+from others import *
+
+class TestStats(unittest.TestCase):
+
+    def test_import_text_as_list_of_strings(self):
+        dossier_corpus = CORPUS_TEST_ROOT
+        texte = None
+
+        for fichier in os.listdir(dossier_corpus) :
+            #On ne veut que les fichiers textes
+            if fichier.endswith('.txt') :
+                # on les importe : on veut que ce soit des listes
+                texte = import_text_as_list_of_words(dossier_corpus+fichier)
+                # on verifie
+                self.assertIsInstance(texte, list)
+
+
+    def test_import_text_as_list_of_words(self):
+        dossier_corpus = CORPUS_TEST_ROOT
+        texte = None
+
+        for fichier in os.listdir(dossier_corpus) :
+            if fichier.endswith('.txt') :
+                texte = import_text_as_list_of_words(dossier_corpus+fichier)
+                self.assertIsInstance(texte, list)
+
+
+if __name__ == '__main__':
+    unittest.main()
