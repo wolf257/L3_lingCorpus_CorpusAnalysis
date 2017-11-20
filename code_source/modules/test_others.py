21b438915522ad9db701a3a95779db8730bc2038
Wolf <wolf257@users.noreply.github.com>
2017-11-20 21:33:51 +0100

    reecriture

--- code_source/modules/test_others.py
+++ code_source/modules/test_others.py
@@ -17,23 +17,41 @@ class TestStats(unittest.TestCase):
         dossier_corpus = CORPUS_TEST_ROOT
         texte = None
 
-        for fichier in os.listdir(dossier_corpus) :
-            #On ne veut que les fichiers textes
-            if fichier.endswith('.txt') :
-                # on les importe : on veut que ce soit des listes
-                texte = import_text_as_list_of_words(os.path.join(dossier_corpus, fichier))
-                # on verifie
-                self.assertIsInstance(texte, list)
+        liste_fichiers = os.listdir(dossier_corpus)
+        
+        if len(liste_fichiers) > 2 :
+            texte_1 = import_text_as_list_of_strings(os.path.join(dossier_corpus, liste_fichiers[1]))
+            self.assertIsInstance(texte_1 , list)
+
+            texte_2 = import_text_as_list_of_strings(os.path.join(dossier_corpus, liste_fichiers[2]))
+            self.assertIsInstance(texte_2 , list)
+
+        # for fichier in os.listdir(dossier_corpus) :
+        #     #On ne veut que les fichiers textes
+        #     if fichier.endswith('.txt') :
+        #         # on les importe : on veut que ce soit des listes
+        #         texte = import_text_as_list_of_words(os.path.join(dossier_corpus, fichier))
+        #         # on verifie
+        #         self.assertIsInstance(texte, list)
 
 
     def test_import_text_as_list_of_words(self):
         dossier_corpus = CORPUS_TEST_ROOT
         texte = None
 
-        for fichier in os.listdir(dossier_corpus) :
-            if fichier.endswith('.txt') :
-                texte = import_text_as_list_of_words(dossier_corpus+fichier)
-                self.assertIsInstance(texte, list)
+        liste_fichiers = os.listdir(dossier_corpus)
+
+        if len(liste_fichiers) > 2 :
+            texte_1 = import_text_as_list_of_words(os.path.join(dossier_corpus, liste_fichiers[1]))
+            self.assertIsInstance(texte_1 , list)
+
+            texte_2 = import_text_as_list_of_words(os.path.join(dossier_corpus, liste_fichiers[2]))
+            self.assertIsInstance(texte_2 , list)
+
+        # for fichier in os.listdir(dossier_corpus) :
+        #     if fichier.endswith('.txt') :
+        #         texte = import_text_as_list_of_words(os.path.join(dossier_corpus, fichier))
+        #         self.assertIsInstance(texte, list)
 
 
 if __name__ == '__main__':
