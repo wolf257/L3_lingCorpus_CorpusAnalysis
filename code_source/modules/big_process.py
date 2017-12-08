4540d94191a1d2f3677b5f7ce3239f3bb4aa48c7
Wolf <wolf257@users.noreply.github.com>
2017-12-08 23:36:14 +0100

    .

--- code_source/modules/big_process.py
+++ code_source/modules/big_process.py
@@ -32,13 +32,11 @@ dossier_corpus = CORPUS_LITTERATURE
 liste_fichiers = others.list_text_in_folder_as_list(dossier_corpus)
 
 def tour_du_corpus(path_to_corpus):
-    name = os.path.basename(os.path.normpath(path_to_corpus))
+    nom_corpus = os.path.basename(os.path.normpath(path_to_corpus))
     print('++++++++++++++++++++++++++++++++++++++++++++++++++')
-    print('\n++Nous travaillons sur le corpus : ' , name)
-    # E0 : lister les fichiers du corpus
-    nom_corpus = str(os.path.relpath(path_to_corpus))
+    print('\n++ Nous travaillons sur le corpus : ' , nom_corpus)
 
-
+    # E0 : CREATION DOSSIER STATISTIQUES a la RACINE DU CORPUS
     print('\n+++ Création du dossier : statistiques')
     try :
         if not os.path.exists(path_to_corpus+'statistiques/') :
@@ -49,76 +47,77 @@ def tour_du_corpus(path_to_corpus):
         print('\tCréation du dossier réussi.')
     path_to_corpus_stat = path_to_corpus+'statistiques/'
 
+    # LISTE FICHIERS DU CORPUS
     files_in_corpus = None
     files_in_corpus = others.list_text_in_folder_as_list(path_to_corpus)
-    nb_fichiers_corpus = int(len(files_in_corpus))
-    nom_corpus = str(os.path.relpath(path_to_corpus))
-    #print('Le corpus ' , os.path.relpath(path_to_corpus) , ' est composé de : ' , len(files_in_corpus) , 'fichiers txt.')
-    #pprint.pprint(files_in_corpus)
+    nb_fichiers_corpus = len(files_in_corpus)
 
-    # E... : creation du fichier contenant les statistiques du corpus
-    nom_fichier_stats_corpus = path_to_corpus_stat+'0_stats_de_'+name+'.txt'
-    # nom_fichier_stats_texte = path_to_corpus+files_in_corpus[i]
-
-    # E.. : Ecriture 'nom corpus' 'longueur initiale' 'composition'
-    print('\n+++ Création du fichier : 0_stats_du_corpus.txt')
+    ########################################
+    # FICHIER PRINCIPAL CORPUS
+    ########################################
+    # CREATION FICHIER STATISTIQUES_CORPUS DANS DOSSIER STATISTIQUES
+        # Ajout titre corpus et contenu
+    nom_fichier_stats_corpus = path_to_corpus_stat+'0_stats_de_'+nom_corpus+'.txt'
+    print('\n++++ Création du fichier : 0_stats_du_corpus.txt')
     try :
         with codecs.open(nom_fichier_stats_corpus, mode='a', encoding='utf8') as file_stat_corpus :
-            file_stat_corpus.write('====================================================')
-            file_stat_corpus.write('\n=== FICHIER CONTENANT LES STATISTIQUES DU CORPUS ===')
-            file_stat_corpus.write('\n====================================================')
+            file_stat_corpus.write('\n\n{}\n{} FICHIER CONTENANT STATISTIQUES DE CORPUS {}\n{}'.format('='*60 , '='*10 , '='*10 , '='*60))
 
-            file_stat_corpus.write('\n\n==========================')
-            file_stat_corpus.write('\n=== PRESENTATION ===')
-            file_stat_corpus.write('\n========================')
+            file_stat_corpus.write('\n\n{}\n{} PRESENTATION {}\n{}'.format('='*30 , '='*7 , '='*7 , '='*30))
 
-            file_stat_corpus.write('\n\n Le corpus ')
-            file_stat_corpus.write(nom_corpus)
-            file_stat_corpus.write(' contient : ')
-            file_stat_corpus.write(str(nb_fichiers_corpus))
-            file_stat_corpus.write( ' fichiers txt.\n')
+            file_stat_corpus.write('\n\nLe corpus {} contient {} fichiers txt : '.format(nom_corpus , nb_fichiers_corpus))
 
             for file in files_in_corpus :
-                file_stat_corpus.write('\n'+file)
+                file_stat_corpus.write('\n - {}'.format(file))
     except :
         print('\tPROBLEME LORS DE LA CREATION DU FICHIER.')
     else :
         print('\tCréation du fichier réussi.\n')
 
-    # E... : creation dES fichiers contenant les statistiques de chaque texte
 
-    with codecs.open(nom_fichier_stats_corpus, mode='a', encoding='utf8') as file_stat_corpus :
-        for file in files_in_corpus :
-            print('+++ Création du fichier : ' , '1_stats_de_'+file)
-            try :
-                path_file = os.path.join(path_to_corpus, file)
-                nom_fichier_stats_texte = path_to_corpus_stat+'1_stats_de_'+file
+    ########################################
+    # FICHIERs INDIVIDUELs TEXTES
+    ########################################
+    # CREATION FICHIER STATISTIQUES_TEXTE DANS DOSSIER STATISTIQUES
+        # Ajout titre texte
+    for file in files_in_corpus :
+        print('++++ Création du fichier : ' , '1_stats_de_'+file)
+        try :
+            path_file = os.path.join(path_to_corpus, file)
+            nom_fichier_stats_texte = path_to_corpus_stat+'1_stats_de_'+file
 
-            # E.. : Ecriture 'nom texte' 'longueur initiale'
-                with codecs.open(nom_fichier_stats_texte, mode='a', encoding='utf8') as file_stat_file :
+        # E.. : Ecriture 'nom texte' 'longueur initiale'
+            with codecs.open(nom_fichier_stats_texte, mode='a', encoding='utf8') as file_stat_file :
 
-                    file_stat_file.write('====================================================')
-                    file_stat_file.write('\n=== FICHIER CONTENANT LES STATISTIQUES DE TEXTE ===')
-                    file_stat_file.write('\n====================================================')
+                file_stat_file.write('\n\n{}\n{} FICHIER CONTENANT STATISTIQUES DU TEXTE : \n\t\t {} {}\n{}'.format('='*80 , '='*10 , file , '='*10 , '='*80))
 
-                    file_stat_file.write('\n\n==========================')
-                    file_stat_file.write('\n=== PRESENTATION ===')
-                    file_stat_file.write('\n========================')
+                file_stat_file.write('\n\n{}\n{} PRESENTATION {}\n{}'.format('='*30 , '='*7 , '='*7 , '='*30))
 
-                    file_stat_file.write('\n\n Le Text ')
-                    file_stat_file.write(file)
-                    file_stat_file.write(' contient : ')
-                    file_stat_file.write( ' xxx lignes.\n')
-            except :
-                print('\tPROBLEME LORS DE LA CREATION DU FICHIER.')
-            else :
-                print('\tCréation du fichier réussi.')
-        print('\n')
+                #file_stat_file.write('\n\nLe texte {} contient {} lignes.'.format(file, 'xxx'))
+        except :
+            print('\tPROBLEME LORS DE LA CREATION DU FICHIER.')
+        else :
+            print('\tCréation du fichier réussi.')
+    print('\n')
+
+    ########################################
+    # DISTRIBUTION FICHIERs INDIVIDUELs TEXTES
+    ########################################
+    for file in files_in_corpus :
+        path_file = os.path.join(path_to_corpus, file)
+        #IMPORTER LE TEXTE EN UNE STR
+        file_as_string = others.import_text_as_one_string(path_file)
+        # ENLEVER LES 3 POINTS : STR
+        string_2 = ponctuation_texte.all_three_points_become_one_string_from_string(file_as_string)
+        # CONVERTIR EN LISTE : LIST
+        liste_1 = string_2.split()
+        # SEPARER LA PONCTUATION DES MOTS LIST
+        liste_2 = ponctuation_texte.punctuation_sep_word_list_from_list(liste_1)
 
     #creer un fichier stats_corpus
     # mettre dedans :
-       # nombre text
-       # nom des text
+       # nombre text DONE
+       # nom des text DONE
 
     # dans chacun des textes :
         # creer fichiers text_stats_base
